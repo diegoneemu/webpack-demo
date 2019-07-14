b751c6ff0b72413bb401e83eb74c675b92b9e9bc
Diego Augusto <43221531+diegoneemu@users.noreply.github.com>
2019-07-14 18:17:21 -0300

    Asset Management (#1)
    * include loaders and config to load assets
    
    * include dist folder in gitignore
    
    * remove dist folder from project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./images/webpack.svg";
+import Data from "./data/data.xml";
 
 function component() {
   const element = document.createElement("div");
 
+  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // add the image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
